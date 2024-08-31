432f63f181b69903529fba1d53f38269b07a3e2c
mihailhr <mishohristov3@gmail.com>
2024-08-31 23:52:53 +0300

    Added random nationality and basic stats

--- app.js
+++ app.js
@@ -20,41 +20,92 @@ const genderSelectPage = `
         <button type="submit">Continue</button>
     </form>
 `;
+const nationalityPage = `
+<h1>You were born in <span id="nationality"></span>.</h1>
+`;
 
-const root=document.getElementById("content")
-let username
-let gender
+const root = document.getElementById("content");
+let username;
+let gender;
+let health=100
+let intelligence=40
+let athleticism=40
+let sociability=50
+let wealth=0
+const allCountries = [
+  // Africa
+  "Nigeria",
+  "South Africa",
+  "Egypt",
+  "Kenya",
+  "Ghana",
 
+  // Asia
+  "China",
+  "India",
+  "Japan",
+  "South Korea",
+  "Thailand",
 
+  // Europe
+  "United Kingdom",
+  "Germany",
+  "France",
+  "Italy",
+  "Spain",
 
+  // North America
+  "United States",
+  "Canada",
+  "Mexico",
 
+  // South America
+  "Brazil",
+  "Argentina",
+  "Colombia",
+  "Chile",
 
-root.innerHTML=welcomePage
+  // Oceania
+  "Australia",
+  "New Zealand",
+  "Fiji",
+];
+const countriesLength = allCountries.length;
+let nationality;
+
+root.innerHTML = welcomePage;
 setTimeout(() => {
-    root.innerHTML=chooseNamePage
-    const form=document.getElementsByTagName("form")[0]
-    form.addEventListener("submit",chooseName)
+  root.innerHTML = chooseNamePage;
+  const form = document.getElementsByTagName("form")[0];
+  form.addEventListener("submit", chooseName);
 }, 4000);
-function chooseName(e){
-    e.preventDefault()
-    const nameField=document.getElementById("username")
-    username=nameField.value
-    chooseGender()
+function chooseName(e) {
+  e.preventDefault();
+  const nameField = document.getElementById("username");
+  username = nameField.value;
+  renderChooseGenderPage();
 }
-function chooseGender(){
-    // e.preventDefault()
-    root.innerHTML=genderSelectPage
-    const usernameField=document.getElementById("username")
-    usernameField.textContent=username
-    const form=document.getElementsByTagName("form")[0]
-    form.addEventListener("submit",makeFirstDecision)
-}
-
-
-function makeFirstDecision(e){
-e.preventDefault()
-const selectedGenderField=document.getElementById("genderSelect")
-gender=selectedGenderField.value
-console.log(gender)
+function renderChooseGenderPage() {
+  // e.preventDefault()
+  root.innerHTML = genderSelectPage;
+  const usernameField = document.getElementById("username");
+  usernameField.textContent = username;
+  const form = document.getElementsByTagName("form")[0];
+  form.addEventListener("submit", chooseGender);
 }
 
+function chooseGender(e) {
+  e.preventDefault();
+  const selectedGenderField = document.getElementById("genderSelect");
+  gender = selectedGenderField.value;
+  console.log(gender);
+  getRandomNationality();
+}
+
+function getRandomNationality() {
+  const randomCountryIndex = Math.floor(Math.random() * countriesLength);
+  nationality = allCountries[randomCountryIndex];
+  root.innerHTML = nationalityPage;
+  const nationalitySpan = document.getElementById("nationality");
+  nationalitySpan.textContent = nationality;
+}
